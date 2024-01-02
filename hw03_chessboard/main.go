ea6488f3922127e5039b6c847aba45f97d560533
”Vladislav <filatov_vn@magnit.ru>
2024-01-02 20:48:02 +0300

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Scanf("%d", &size)
+
+	for x := 1; x <= size; x++ {
+		if x%2 == 0 {
+			fmt.Printf(" ")
+		}
+		for y := 1; y <= size-1; y++ {
+			if y%2 != 0 {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf(" ")
+			}
+		}
+		if x%2 != 0 {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("\n")
+	}
 }
