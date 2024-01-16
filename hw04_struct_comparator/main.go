12a17fe36f6eb8a2221c4454b2e1e15ce4edc923
”Vladislav <filatov_vn@magnit.ru>
2024-01-16 15:10:25 +0300

    fix_1

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -2,14 +2,8 @@ package main
 
 import "fmt"
 
-const (
-	year = "year"
-	size = "size"
-	rate = "rate"
-)
-
 type Book struct {
-	id     int64
+	isbn   int64
 	title  string
 	author string
 	year   int
@@ -17,66 +11,76 @@ type Book struct {
 	rate   float32
 }
 
-// getters
-
-func (b *Book) BookID() int64 {
-	return b.id
+type Comparator struct {
+	fieldCompare string
 }
 
-func (b *Book) BookTitle() string {
+// getters
+
+func (b *Book) ISBN() int64 {
+	return b.isbn
+}
+
+func (b *Book) Title() string {
 	return b.title
 }
 
-func (b *Book) BookAuthor() string {
+func (b *Book) Author() string {
 	return b.author
 }
 
-func (b *Book) BookYear() int {
+func (b *Book) Year() int {
 	return b.year
 }
 
-func (b *Book) BookSize() int {
+func (b *Book) Size() int {
 	return b.size
 }
 
-func (b *Book) BookRate() float32 {
+func (b *Book) Rate() float32 {
 	return b.rate
 }
 
 // setters
 
-func (b *Book) SetBookID(id int64) {
-	b.id = id
+func (b *Book) SetISBN(isbn int64) {
+	b.isbn = isbn
 }
 
-func (b *Book) SetBookTitle(title string) {
+func (b *Book) SetTitle(title string) {
 	b.title = title
 }
 
-func (b *Book) SetBookAuthor(author string) {
+func (b *Book) SetAuthor(author string) {
 	b.author = author
 }
 
-func (b *Book) SetBookYear(year int) {
+func (b *Book) SetYear(year int) {
 	b.year = year
 }
 
-func (b *Book) SetBookSize(size int) {
+func (b *Book) SetSize(size int) {
 	b.size = size
 }
 
-func (b *Book) SetBookRate(rate float32) {
+func (b *Book) SetRate(rate float32) {
 	b.rate = rate
 }
 
-func CompareBooks(b1 *Book, b2 *Book, compareField string) bool {
-	switch compareField {
-	case year:
-		return b1.BookYear() > b2.BookYear()
-	case size:
-		return b1.BookSize() > b2.BookSize()
-	case rate:
-		return b1.BookRate() > b2.BookRate()
+func NewComparator(fieldCompare string) *Comparator {
+	comparator := Comparator{}
+	comparator.fieldCompare = fieldCompare
+	return &comparator
+}
+
+func (c *Comparator) Compare(bookOne, bookTwo *Book) bool {
+	switch c.fieldCompare {
+	case "year":
+		return bookOne.Year() > bookTwo.Year()
+	case "size":
+		return bookOne.Size() > bookTwo.Size()
+	case "rate":
+		return bookOne.Rate() > bookTwo.Rate()
 	default:
 		return false
 	}
@@ -86,28 +90,33 @@ func main() {
 	bk := Book{}
 	bk2 := Book{}
 
-	bk.SetBookID(1)
-	bk.SetBookTitle("Книга 1")
-	bk.SetBookAuthor("Какой-то автор")
-	bk.SetBookYear(1996)
-	bk.SetBookSize(56)
-	bk.SetBookRate(7.8)
+	bk.SetISBN(1)
+	bk.SetTitle("Книга 1")
+	bk.SetAuthor("Какой-то автор")
+	bk.SetYear(1996)
+	bk.SetSize(56)
+	bk.SetRate(7.8)
 
-	bk2.SetBookID(1)
-	bk2.SetBookTitle("Книга 2")
-	bk2.SetBookAuthor("Какой-то автор")
-	bk2.SetBookYear(1997)
-	bk2.SetBookSize(51)
-	bk2.SetBookRate(7.3)
+	bk2.SetISBN(2)
+	bk2.SetTitle("Книга 2")
+	bk2.SetAuthor("Какой-то автор")
+	bk2.SetYear(1997)
+	bk2.SetSize(51)
+	bk2.SetRate(7.3)
 
-	// fmt.Println(bk.BookID())
-	// fmt.Println(bk.BookTitle())
-	// fmt.Println(bk.BookAuthor())
-	// fmt.Println(bk.BookYear())
-	// fmt.Println(bk.BookSize())
-	// fmt.Println(bk.BookRate())
+	fmt.Println(bk.ISBN())
+	fmt.Println(bk.Title())
+	fmt.Println(bk.Author())
+	fmt.Println(bk.Year())
+	fmt.Println(bk.Size())
+	fmt.Println(bk.Rate())
 
-	fmt.Println(CompareBooks(&bk, &bk2, year))
-	fmt.Println(CompareBooks(&bk, &bk2, size))
-	fmt.Println(CompareBooks(&bk, &bk2, rate))
+	c := NewComparator("year")
+	fmt.Println(c.Compare(&bk, &bk2))
+	c = NewComparator("size")
+	fmt.Println(c.Compare(&bk, &bk2))
+	c = NewComparator("rate")
+	fmt.Println(c.Compare(&bk, &bk2))
+	c = NewComparator("none")
+	fmt.Println(c.Compare(&bk, &bk2))
 }
