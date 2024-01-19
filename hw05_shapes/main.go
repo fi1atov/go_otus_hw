9972457b54a511c31f869528f8f8673cde8728dd
”Vladislav <filatov_vn@magnit.ru>
2024-01-19 18:12:15 +0300

    fix_1

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -23,23 +23,22 @@ type Triangle struct {
 }
 
 func (c *Circle) area() float64 {
-    return math.Pi * c.r*c.r
+	return math.Pi * c.r * c.r
 }
 
 func (r *Rectangle) area() float64 {
-    return r.x * r.y
+	return r.x * r.y
 }
 
 func (t *Triangle) area() float64 {
-    return t.x * t.h / 2
+	return t.x * t.h / 2
 }
 
 func calculateArea(s any) (float64, error) {
 	if myShape, ok := s.(Shape); ok {
 		return myShape.area(), nil
-	} else {
-		return 0, errors.New("Ошибка: переданный объект не является фигурой.")
 	}
+	return 0, errors.New("ошибка: переданный объект не является фигурой")
 }
 
 func main() {
@@ -48,13 +47,13 @@ func main() {
 	t := Triangle{8, 6}
 	s := "Hello"
 
-	c_res, _ := calculateArea(&c)
-	r_res, _ := calculateArea(&r)
-	t_res, _ := calculateArea(&t)
+	cRes, _ := calculateArea(&c)
+	rRes, _ := calculateArea(&r)
+	tRes, _ := calculateArea(&t)
 	_, err := calculateArea(&s)
 
-	fmt.Printf("Круг: радиус %d Площадь: %v\n", int(c.r), c_res)
-	fmt.Printf("Прямоугольник: ширина %d, высота %d Площадь: %d\n", int(r.x), int(r.y), int(r_res))
-	fmt.Printf("Треугольник: основание %d, высота %d Площадь: %d\n", int(t.x), int(t.h), int(t_res))
+	fmt.Printf("Круг: радиус %d Площадь: %v\n", int(c.r), cRes)
+	fmt.Printf("Прямоугольник: ширина %d, высота %d Площадь: %d\n", int(r.x), int(r.y), int(rRes))
+	fmt.Printf("Треугольник: основание %d, высота %d Площадь: %d\n", int(t.x), int(t.h), int(tRes))
 	fmt.Println(err)
 }
