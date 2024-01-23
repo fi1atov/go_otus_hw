0a56d4c4dc2bfc303eeaeb3bab77bf7def033b68
”Vladislav <filatov_vn@magnit.ru>
2024-01-23 12:18:06 +0300

    fix_5 methods to package

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
-	"math"
 
 	"github.com/fi1atov/go_otus_hw/hw05_shapes/objects"
 )
 
 type Shape interface {
 	Area() float64
-}
-
-func (c objects.Circle) Area() float64 {
-	return math.Pi * c.R * c.R
-}
-
-func (r objects.Rectangle) Area() float64 {
-	return r.X * r.Y
-}
-
-func (t objects.Triangle) Area() float64 {
-	return t.X * t.H / 2
 }
 
 func calculateArea(s any) (float64, error) {
@@ -32,9 +19,9 @@ func calculateArea(s any) (float64, error) {
 }
 
 func main() {
-	c := objects.Circle{5}
-	r := objects.Rectangle{10, 5}
-	t := objects.Triangle{8, 6}
+	c := objects.Circle{R: 5}
+	r := objects.Rectangle{X: 10, Y: 5}
+	t := objects.Triangle{X: 8, H: 6}
 	s := "Hello"
 
 	cRes, _ := calculateArea(&c)
--- hw05_shapes/objects/objects.go
+++ hw05_shapes/objects/objects.go
@@ -1,5 +1,7 @@
 package objects
 
+import "math"
+
 type Circle struct {
 	R float64
 }
@@ -10,4 +12,16 @@ type Rectangle struct {
 
 type Triangle struct {
 	X, H float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.R * c.R
+}
+
+func (r Rectangle) Area() float64 {
+	return r.X * r.Y
+}
+
+func (t Triangle) Area() float64 {
+	return t.X * t.H / 2
 }
