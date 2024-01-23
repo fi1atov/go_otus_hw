ac7a09e2ba6e796599696dfecc55f44d959db306
”Vladislav <filatov_vn@magnit.ru>
2024-01-23 10:01:32 +0300

    fix_4 objects to package

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -4,21 +4,23 @@ import (
 	"errors"
 	"fmt"
 	"math"
+
+	"github.com/fi1atov/go_otus_hw/hw05_shapes/objects"
 )
 
 type Shape interface {
 	Area() float64
 }
 
-func (c *Circle) Area() float64 {
+func (c objects.Circle) Area() float64 {
 	return math.Pi * c.R * c.R
 }
 
-func (r *Rectangle) Area() float64 {
+func (r objects.Rectangle) Area() float64 {
 	return r.X * r.Y
 }
 
-func (t *Triangle) Area() float64 {
+func (t objects.Triangle) Area() float64 {
 	return t.X * t.H / 2
 }
 
@@ -30,9 +32,9 @@ func calculateArea(s any) (float64, error) {
 }
 
 func main() {
-	c := Circle{5}
-	r := Rectangle{10, 5}
-	t := Triangle{8, 6}
+	c := objects.Circle{5}
+	r := objects.Rectangle{10, 5}
+	t := objects.Triangle{8, 6}
 	s := "Hello"
 
 	cRes, _ := calculateArea(&c)
--- hw05_shapes/objects/objects.go
+++ hw05_shapes/objects/objects.go
@@ -1,4 +1,4 @@
-package main
+package objects
 
 type Circle struct {
 	R float64
