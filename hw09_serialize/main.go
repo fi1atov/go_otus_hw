72782bc7b96506afc7edd8c78080576fea88ab68
”Vladislav <filatov_vn@magnit.ru>
2024-02-19 13:28:05 +0300

    fix_1

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -7,7 +7,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type Book struct {
+type BookLocal struct {
 	Isbn   int64
 	Title  string
 	Author string
@@ -18,15 +18,15 @@ type Book struct {
 
 type Marshaller interface {
 	MarshalJSON() []byte
-	// MarshalPROTO() []byte
+	MarshalPROTO() []byte
 }
 
 type Unmarshaller interface {
 	UnmarshalJSON()
-	// UnmarshallPROTO()
+	UnmarshalPROTO()
 }
 
-func (b *Book) MarshalJSON() []byte {
+func (b *BookLocal) MarshalJSON() []byte {
 	res, err := json.Marshal(b)
 	if err != nil {
 		panic(err)
@@ -34,26 +34,28 @@ func (b *Book) MarshalJSON() []byte {
 	return res
 }
 
-func (b *Book) UnmarshalJSON(data []byte) {
+func (b *BookLocal) UnmarshalJSON(data []byte) {
 	if err := json.Unmarshal(data, &b); err != nil {
 		panic(err)
 	}
 }
 
-// func (b *Book) MarshalPROTO() []byte {
-// 	res, err := proto.Marshal(b)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return res
-// }
+func (b *Book) MarshalPROTO() []byte {
+	res, err := proto.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
 
-// func UnmarshallPROTO([]byte) []Book {
-// 	return nil
-// }
+func (b *Book) UnmarshalPROTO(data []byte) {
+	if err := proto.Unmarshal(data, b); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	book := Book{
+	book := BookLocal{
 		Isbn:   1,
 		Title:  "Книга",
 		Author: "Автор",
@@ -61,7 +63,16 @@ func main() {
 		Size:   145,
 		Rate:   0.8,
 	}
-	bookSecond := Book{}
+	bookSecond := BookLocal{}
+	bookProto := &Book{
+		Isbn:   1,
+		Title:  "Книга",
+		Author: "Автор",
+		Year:   1996,
+		Size:   145,
+		Rate:   0.8,
+	}
+	bookProtoSecond := &Book{}
 
 	fmt.Printf("Объект book: %v\n", book)
 	fmt.Printf("Объект bookSecond: %v\n", bookSecond)
@@ -72,6 +83,14 @@ func main() {
 	fmt.Printf("book в JSON: %v\n", string(res))
 	fmt.Printf("Объект bookSecond: %v\n", bookSecond)
 
-	protoRes, _ := proto.Marshal(&book)
+	fmt.Printf("-----------------------------------------------\n")
+
+	fmt.Printf("Объект bookProto: %v\n", bookProto)
+	fmt.Printf("Объект bookProtoSecond: %v\n", bookProtoSecond)
+
+	protoRes := bookProto.MarshalPROTO()
+	bookProtoSecond.UnmarshalPROTO(protoRes)
+
 	fmt.Printf("Объект protoRes: %v\n", protoRes)
+	fmt.Printf("Объект bookProtoSecond: %v\n", bookProtoSecond)
 }
